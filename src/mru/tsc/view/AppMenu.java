1b6ffa4a6fe7d84b78e53e1339eada26a03f1ff6
nashdashin <srivastavanikhil226@yahoo.ca>
2022-10-03 09:03:14 -0600

    changes for the app menu

--- src/mru/tsc/view/AppMenu.java
+++ src/mru/tsc/view/AppMenu.java
@@ -1,21 +1,19 @@
 package mru.tsc.view;
-
-
 import java.util.Scanner;
-
 
 public class AppMenu {
 	
 	Scanner myObj;
-	
-	
+	Scanner dealObj;
+
 	public AppMenu() {
 		
 		myObj = new Scanner(System.in);
-		
+		dealObj = new Scanner(System.in);
+				
+	
 	}
 
-	
 	public String showMainMenu() {
 		
 		System.out.println("Select one of these options:");
@@ -26,6 +24,15 @@ public class AppMenu {
 		String mainMenuChoice = myObj.nextLine();
 		return mainMenuChoice;		
 		
+	}
+	
+	public int playWarGameDealer() {
+		
+		System.out.println("**WELCOME TO WARGAME!!");
+		System.out.println("");
+		System.out.println("How many cards to deal: ");
+		int dealerChoice = dealObj.nextInt();
+		return dealerChoice;
 	}
 	
 }
